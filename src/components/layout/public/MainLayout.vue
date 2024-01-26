8589dac2b16309f84cf6afff09678dc59ba66f14
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2024-01-26 16:50:43 +0900

    サイドメニュー作成

---
+++ src/components/layout/public/MainLayout.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import SideMenu from './SideMenu.vue';
+import TheHeader from './TheHeader.vue';
+
+const HEADER_HEIGHT = "50px"
+
+const slim = ref(false)
+const toggle = () => slim.value = !slim.value
+</script>
+
+<template>
+  <div id="wrapper" :class="{ slim }">
+    <TheHeader />
+    <SideMenu :header-height="HEADER_HEIGHT" :slim="slim" @toggle="toggle" />
+    <div class="main">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#wrapper {
+  min-width: 100vw;
+  display: grid;
+  grid-auto-rows: v-bind(HEADER_HEIGHT) 1fr;
+  grid-template-columns: 200px 1fr;
+  grid-template-areas:
+    "header header"
+    "aside main"
+    "aside main"
+  ;
+
+  &.slim {
+    grid-template-columns: 80px 1fr;
+  }
+}
+
+.main {
+  grid-area: main;
+}
+</style>
---
+++ src/components/layout/public/TheHeader.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="header">
+    this is header
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background: whitesmoke;
+  grid-area: header;
+}
+</style>
