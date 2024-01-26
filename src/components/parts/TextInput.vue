7b21c954685b27196f5e80c9924f7d729b1fe923
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2024-01-26 15:03:17 +0900

    未認証レイアウト作成

---
+++ src/components/parts/TextInput.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+defineProps<{ label: string }>()
+</script>
+
+<template>
+  <label>{{ label }}</label>
+  <input autocomplete="off" v-bind="$attrs" />
+</template>
+
+<style scoped></style>
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { PublicLayout } from '@/components/layout/public';
+import FlexBox from "@/components/parts/FlexBox.vue";
+import InputForm from '@/components/parts/InputForm.vue';
+import TextInput from '@/components/parts/TextInput.vue';
+import TheButton from '@/components/parts/button/TheButton.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const onSubmit = () => {
+  return new Promise<void>(resolve => {
+    setTimeout(() => {
+      router.replace({ name: "home" })
+      resolve()
+    }, 1 * 1000)
+  })
+}
+</script>
+
+<template>
+  <PublicLayout>
+    <main>
+      <FlexBox class="column">
+        <FlexBox class="column header">
+          <h1>This is Header Area</h1>
+        </FlexBox>
+        <InputForm @submit.prevent id="form">
+          <TextInput label="id" />
+          <TextInput label="password" type="password" />
+          <TheButton :command="onSubmit">submit</TheButton>
+        </InputForm>
+      </FlexBox>
+    </main>
+  </PublicLayout>
+</template>
+
+<style scoped>
+.header {
+  height: 100px;
+  place-content: center;
+}
+</style>
