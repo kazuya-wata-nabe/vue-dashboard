2bb32dcb0332a98ee33bced80fc00ffcd889b36c
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2024-03-16 09:32:22 +0900

    roleの受け渡し

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import type { Role } from "@/provider/auth/model/role"
+
+defineProps<{
+  role: Role | undefined
+}>()
+</script>
+
 <template>
-  <component :is="$route.meta.layout">
+  <component :is="$route.meta.layout" :role="role">
     <slot></slot>
   </component>
 </template>
