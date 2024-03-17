1e7e211832b0146851b52914689c8a247c5d0594
kazuya-wata-nabe <kazuya-wata-nabe@users.noreply.github.com>
2024-03-17 01:50:29 +0000

    Apply Prettier Change

--- setupFile.ts
+++ setupFile.ts
@@ -1,6 +1,6 @@
-import "@testing-library/jest-dom/vitest"
 // Storybook's preview file location
 import { setProjectAnnotations } from "@storybook/vue3"
+import "@testing-library/jest-dom/vitest"
 import globalStorybookConfig from "./.storybook/preview"
 
 setProjectAnnotations(globalStorybookConfig)
