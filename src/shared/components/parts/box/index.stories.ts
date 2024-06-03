521ec75504a08f303195c27d21d0a82aa8739d7c
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2024-06-03 22:52:46 +0900

    update

---
+++ src/shared/components/parts/box/index.stories.ts
@@ -0,0 +1,63 @@
+import { type StoryObj } from "@storybook/vue3"
+import { FlexCol, FlexRow } from "@/shared/components/parts"
+import "@/shared/components/parts/index.css"
+
+const meta = {
+  tags: ["autodocs", "!dev"],
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const _FlexCol: Story = {
+  render: () => ({
+    components: { FlexCol, FlexRow },
+    template: `
+      <FlexRow gap="8">
+        <FlexCol gap="8">
+          gap: 8
+          <div>hoge</div>
+        </FlexCol>
+        <FlexCol gap="16">
+          gap: 16
+          <div>hoge</div>
+        </FlexCol>
+        <FlexCol gap="24">
+          gap: 24
+          <div>hoge</div>
+        </FlexCol>
+        <FlexCol gap="32">
+          gap: 32
+          <div>hoge</div>
+        </FlexCol>
+      </FlexRow>
+    `,
+  }),
+}
+
+export const _FlexRow: Story = {
+  render: () => ({
+    components: { FlexRow },
+    template: `
+      <div class="canvas col">
+        <FlexRow gap="8">
+          gap: &nbsp;&nbsp;8
+          <div>hoge</div>
+        </FlexRow>
+        <FlexRow gap="16">
+          gap: 16
+          <div>hoge</div>
+        </FlexRow>
+        <FlexRow gap="24">
+          gap: 24
+          <div>hoge</div>
+        </FlexRow>
+        <FlexRow gap="32">
+          gap: 32
+          <div>hoge</div>
+        </FlexRow>
+      </div>
+    `,
+  }),
+}
