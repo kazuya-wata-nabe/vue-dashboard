14025c852ac31fba559a566f15a198f42ca1f087
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2024-08-26 09:19:45 +0900

    add @vitest/eslint-plugin (#23)

--- src/shared/lib/date.test.ts
+++ src/shared/lib/date.test.ts
@@ -1,26 +1,26 @@
-import { describe, expect, test } from "vitest"
+import { describe, expect, it } from "vitest"
 import { isValid } from "./date"
 
 describe("parseの入力形式", () => {
-  test("2021-01-03は有効", () => {
+  it("2021-01-03は有効", () => {
     const value = "2021-01-03"
     const actual = isValid(value)
     expect(actual).toBeTruthy()
   })
 
-  test("2021/01/03は無効", () => {
+  it("2021/01/03は無効", () => {
     const value = "2021/01/03"
     const actual = isValid(value)
     expect(actual).toBeFalsy()
   })
 
-  test("2021_01_03は無効", () => {
+  it("2021_01_03は無効", () => {
     const value = "2021_01_03"
     const actual = isValid(value)
     expect(actual).toBeFalsy()
   })
 
-  test("2021.01.03は無効", () => {
+  it("2021.01.03は無効", () => {
     const value = "2021.01.03"
     const actual = isValid(value)
     expect(actual).toBeFalsy()
