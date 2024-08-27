0de92bc0f996e56430074e9cda8c813e6e7cfb87
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2024-08-27 21:52:28 +0900

    update

--- src/views/gantt-chart/components/task-bar.vue
+++ src/views/gantt-chart/components/task-bar.vue
@@ -11,15 +11,18 @@ const props = defineProps<{
 // TODO: watch reactivity
 const state = ref({ width: props.width, offset: props.offset })
 const container = ref<HTMLDivElement>()
+const bar = ref<HTMLDivElement>()
+const east = ref<HTMLDivElement>()
 
 const size = computed(() => `${state.value.width / 100}px`)
 const left = computed(() => `${state.value.offset}px`)
 
-let mousedown = false
+let mode: "move" | "expand-west" | "expand-east" | ""
 let point = 0
+let temp = 0
 
 const onMouseMove = (event: MouseEvent) => {
-  if (!mousedown) return
+  if (mode !== "move") return
   if (!point) {
     point = event.offsetX
   }
@@ -29,34 +32,84 @@ const onMouseMove = (event: MouseEvent) => {
   const value = event.pageX - shiftX - point + parentLeft
   state.value.offset = value > -1 ? value : 0
 }
+
+const expand = (event: MouseEvent) => {
+  if (mode === "expand-east" && east.value && bar.value) {
+    if (!temp) {
+      temp = bar.value.offsetWidth
+    }
+    const shiftX = container.value?.offsetLeft ?? 0
+    const value = event.pageX - shiftX - temp
+    console.debug(value)
+    bar.value.style.width = `${value}px`
+  }
+}
+
+const onMouseUp = () => {
+  mode = ""
+  temp = 0
+  point = 0
+}
 </script>
 
 <template>
-  <div class="task-bar-container" ref="container">
+  <div class="task-bar-container" ref="container" @mousemove="expand" role="button" tabindex="0">
     <div
-      class="task-bar"
-      @mousemove="onMouseMove"
-      @mousedown="() => (mousedown = true)"
-      @mouseup="() => (mousedown = false)"
-      @focus="() => {}"
+      class="position edge"
       role="button"
       tabindex="0"
+      @mousedown="() => (mode = 'expand-west')"
+      @mouseup="onMouseUp"
+    ></div>
+    <div
+      class="task-bar position"
+      @mousemove="onMouseMove"
+      @mousedown="() => (mode = 'move')"
+      @mouseup="onMouseUp"
+      role="button"
+      tabindex="0"
+      ref="bar"
+    ></div>
+    <div
+      class="position edge reverse"
+      ref="east"
+      role="button"
+      tabindex="0"
+      @mousedown="() => (mode = 'expand-east')"
+      @mouseup="onMouseUp"
     ></div>
   </div>
 </template>
 
 <style scoped>
+.position {
+  position: relative;
+  left: v-bind(left);
+}
+
+.edge {
+  min-width: 10px;
+  background-color: silver;
+  cursor: w-resize;
+  &:hover {
+    opacity: 0.8;
+  }
+  &.reverse {
+    cursor: e-resize;
+  }
+}
+
 .task-bar-container {
   height: 30px;
   padding: 1px 0;
+  display: flex;
+  flex-direction: row;
 }
 
 .task-bar {
   height: 100%;
-  width: calc(40 * v-bind(size));
+  width: calc(40 * v-bind(size) - 20px);
   background-color: silver;
-  position: relative;
-  left: v-bind(left);
   cursor: pointer;
 }
 </style>
