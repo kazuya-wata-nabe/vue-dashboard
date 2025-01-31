4d4d16f32aeb10770b1f97d465da3597910963b1
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2025-01-31 16:07:59 +0900

    update

--- docs/convert-inline.ts
+++ docs/convert-inline.ts
@@ -2,6 +2,7 @@ import { JSDOM } from "jsdom"
 import { writeFile, readFile, readdir } from "node:fs/promises"
 
 type Dto = {
+  source: string
   path: string
   depth: number
 }
@@ -11,6 +12,7 @@ const getHtmlFileRecursive = async (source: string): Promise<Dto[]> => {
   const htmlList = fileList.filter((file) => file.endsWith(".html"))
 
   return htmlList.map((html) => ({
+    source: html,
     path: source + "/" + html,
     depth: html.split("/").length - 1,
   }))
@@ -27,7 +29,12 @@ const convert = async (dto: Dto) => {
     element.href = prefix + element.href
   }
 
-  for (const element of getAll<HTMLAnchorElement>("a.VPLink")) {
+  const anchors = [
+    ...[...getAll<HTMLAnchorElement>("a.VPLink")].filter((el) => el.text !== "画面遷移図"),
+    ...getAll<HTMLAnchorElement>("a.VPButton"),
+    ...getAll<HTMLAnchorElement>("div.VPNavBarTitle > a.title"),
+  ]
+  for (const element of anchors) {
     const hasExt = element.href.endsWith(".html")
     const href = hasExt ? element.href.slice(0, -5) + "/" : element.href
     element.href = prefix + href + "index.html"
@@ -41,9 +48,11 @@ const convert = async (dto: Dto) => {
 }
 
 const dir = import.meta.dirname + "/.vitepress/dist"
-const dtos = await getHtmlFileRecursive(dir)
+const items = await getHtmlFileRecursive(dir)
+const ignores = new Set(["404.html", "README.html"])
+const dtos = items.filter((item) => !ignores.has(item.source))
 
 for (const dto of dtos) {
-  // console.debug(dto.path)
+  console.debug(dto.source)
   await convert(dto)
 }
