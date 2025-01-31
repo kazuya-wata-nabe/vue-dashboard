eeddc981c30b84ca771f2cef66eb95a73244125b
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2025-01-31 17:48:47 +0900

    update

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,6 @@ export default function middleware(request: Request) {
     const basicAuth = authorizationHeader.split(" ")[1]
     const [user, password] = atob(basicAuth).toString().split(":")
 
-    console.debug(user, process.env.BASIC_AUTH_USER)
     if (user === process.env.BASIC_AUTH_USER && password === process.env.BASIC_AUTH_PASSWORD) {
       return next()
     }
