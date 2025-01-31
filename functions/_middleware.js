324e18d3a954a45b9979c6880be240903c22f176
kazuya-wata-nabe <kazuya.watanabe@sorich.jp>
2025-01-31 18:41:54 +0900

    fix: env context

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -6,7 +6,7 @@ async function errorHandling(context) {
   }
 }
 
-async function handleRequest({ next, request }) {
+async function handleRequest({ next, request, env }) {
   // The "Authorization" header is sent when authenticated.
   if (request.headers.has("Authorization")) {
     const Authorization = request.headers.get('Authorization')
@@ -37,11 +37,11 @@ async function handleRequest({ next, request }) {
     const user = decoded.substring(0, index);
     const pass = decoded.substring(index + 1);
 
-    if (context.cloudflare.env.BASIC_AUTH_USER !== user) {
+    if (env.BASIC_AUTH_USER !== user) {
       return new Response('Invalid credentials.', { status: 401 })
     }
 
-    if (context.cloudflare.env.BASIC_AUTH_PASSWORD !== pass) {
+    if (env.BASIC_AUTH_PASSWORD !== pass) {
       return new Response('Invalid credentials.', { status: 401 })
     }
 
